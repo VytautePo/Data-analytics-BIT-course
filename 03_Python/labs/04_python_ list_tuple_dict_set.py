79006066d5ac419f1e3e49334959fee2ce32a432
VytautePo <Vytaute.Povilanskaite@gmail.com>
2026-02-04 10:50:05 +0200

    New lab added

---
+++ 03_Python/labs/04_python_ list_tuple_dict_set.py
@@ -0,0 +1,43 @@
+## Section 1: Lists, Tuples, Dicts, and Sets
+
+#1. Create a list of 5 product names; print the first and last using indices.
+
+#2.Create a tuple of customer names. Convert it to a list, add a new name, and convert it back to a tuple.
+
+#3.You have a list of prices [10, 20, 15, 30]. Convert it to a set and show how many unique prices there are using set() and len().
+
+#4.Create a dictionary (dict) with product names as keys and sales counts as values. Change the value of one product using indexing.
+
+#5.You have list = ["A", "B", "C"] and set = {"B", "C", "D"}. Find the common elements using intersection.
+
+#6.Create a tuple with monthly profits (12 values). Calculate the average profit using sum() and len().
+
+#7.Create a dict where keys are months and values are profits. Calculate the sum of all months.
+
+#8.Create a list of purchase dates as strings (e.g., '2023-01-01') and convert them to datetime objects using strptime.
+
+#9.Calculate how many days have passed from each purchase date until today.
+
+#10.Create a dict where keys are product names and values are lists of monthly sales. Find the total sum for each product using a for loop and sum().
+
+### Section 2: Function Creation (Data Analytics)
+
+#1. Function that returns a number multiplied by 2.
+
+#2. Function to calculate Conversion Rate: (buyers / users) * 100.
+
+#3. Function to calculate CTR: (clicks / impressions) * 100.
+
+#4. Function to calculate the average of a list.
+
+#5. Function that returns the difference between two numbers (to calculate monthly change).
+
+#6. Function that takes lists of revenue and expenses and returns the net profit for each period.
+
+#7. Function that counts how many times the value 'inactive' appears in a list.
+
+#8. Function that takes a dict of sales by region and returns the region with the maximum sales.
+
+#9. Function that returns True if profit is positive, otherwise False.
+
+#10. Function that converts a list of date strings into datetime objects.
